2c082a42daddad3f48c40670182b7930dd8b03c4
lhaisong <liuhaisong@skyworth,com>
2020-08-21 17:45:36 +0800

    submit c note

---
+++ C语言学习/first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+const int LENGTH=10;
+const int WIDTH=5;
+
+int main()
+{
+    int area;
+    area=LENGTH*WIDTH;
+    printf("first program");
+    printf("\nhello\tworld\n\n");
+    printf("area is:%d",area);
+}
---
+++ C语言学习/second.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    char str[]="this is a string";
+    puts(str);
+    printf("str:%s",str);
+    return 0;
+}
